f121f865a2cf6d1755b6f23ffdac6d2919cbc7e7
Kenny Su <kfernandosuqiu@gmail.com>
2022-01-21 18:42:25 -0500

    Finishing touches with comments

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -19,10 +19,6 @@ namespace ListManagement // Note: actual namespace depends on the project name.
             {
                 while (input != 7)
                 {
-                    // ToDoList = null;
-                    // ToDoList?.Clear();  // Clears the list and handles error exception
-                    // ToDoList = ToDoList ?? new List<ToDo>();    // Null coalescing operator C#
-
                     nextToDo = new ToDo();
 
                     // Option #1 - Create a task
