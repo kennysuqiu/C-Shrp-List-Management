060cbd84007ecf82e9c7b9aae7425ab050e35b94
Kenny Su <kfernandosuqiu@gmail.com>
2022-02-14 09:46:00 -0500

    Added ItemService

---
+++ MyApp/ItemService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ListManagement.models;
+
+namespace ListManagement
+{
+	public class ItemService
+	{
+		static private List<Item> items;
+
+		static private ItemService instance;
+
+		public List<Item> Items
+		{ 
+			get
+			{
+				return items;
+			} 
+		}
+
+
+		static public ItemService Current
+        {
+			get
+            {
+				if (instance == null)
+				{
+					instance = new ItemService();
+
+				}
+				return instance;
+			}
+        }
+
+		private ItemService()
+        {
+			items = new List<Item>();
+        }
+
+		public void Add(Item i)
+        {
+			items.Add(i);
+        }
+
+		public void Remove(Item i)
+        {
+			items.Remove(i);
+        }
+	}
+}
+
